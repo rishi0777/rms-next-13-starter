96ccb18d2b82e618364f31420e6696cf31859218
Rishi <rishi.misra.777@gmail.com>
2023-07-01 20:28:59 +0530

    feat: baseline structure

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
+// const withTranspileModules = require('next-transpile-modules')([
+//   '@rms-forge/ui-text',
+// ]);
+
+// module.exports = withTranspileModules();
